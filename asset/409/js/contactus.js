f1b8dd7245f8654e36f9e7410a804a07ebe700be
bquesnel <brandon.quesnel@cobornsinc.com>
2017-03-09 10:09:36 -0600

    form scripts

---
+++ asset/409/js/contactus.js
@@ -0,0 +1,16 @@
+var rs6w9nc0gpqsxq;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'coborns',
+'formHash':'rs6w9nc0gpqsxq',
+'autoResize':true,
+'height':'1579',
+'async':true,
+'host':'wufoo.com',
+'header':'show',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { rs6w9nc0gpqsxq = new WufooForm();rs6w9nc0gpqsxq.initialize(options);rs6w9nc0gpqsxq.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');
